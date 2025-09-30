ca6e8232af17fabd9ff6dde2d9308ad2edd5abd1
jaycodev <jason.vilac@gmail.com>
2025-09-30 17:37:33 -0500

    📝 docs(api): add documentation configuration with springdoc-openapi

--- src/main/java/com/codeloop/CodeloopApplication.java
+++ src/main/java/com/codeloop/CodeloopApplication.java
@@ -2,8 +2,17 @@ package com.codeloop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(
+        title = "CodeLoop API",
+        version = "1.0.0",
+        description = "REST API for a web online course management system."
+    )
+)
 public class CodeloopApplication {
 
 	public static void main(String[] args) {
